9b27a6dc6bd6b66ffa4a02e010458bc1ec7ee65d
Martin Štefaňák <stefanak@photoneo.com>
2018-02-25 19:46:36 +0100

    Color aid for finding differences functional

--- find_differences_solver.py
+++ find_differences_solver.py
@@ -12,14 +12,14 @@ translations = []
 index = 0
 for name in images:
     img = cv2.imread(name)    
-    surf = cv2.xfeatures2d.SURF_create(3000)
+    surf = cv2.xfeatures2d.SURF_create(500, upright = True)
     kp2, des2 = surf.detectAndCompute(img, None )
     FLANN_INDEX_KDTREE = 0
     index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
     search_params = dict(checks=50)   # or pass empty dictionary
     flann = cv2.FlannBasedMatcher(index_params,search_params)
     matches = flann.knnMatch(des2,des2,k=2)
-    img2 = img
+    img2 = img.copy()
     file_name= str(index).zfill(2) + '_' + name[14:19]
     file = open('results/'+ file_name + '.csv', 'w')
     file.write('distance-orig;angle-deg\n')
@@ -47,9 +47,8 @@ for name in images:
     translations.append(np.array(zip(distances, angles)))
 
     # find the most dense area of [translation distance, trabslation angle] space
-    hist = plt.hist2d(distances, angles, bins=32)
-    plt.colorbar()
-    plt.show()
+    hist = plt.hist2d(distances, angles, bins=64)
+   
     (x,y) = unravel_index(hist[0].argmax(), hist[0].shape)
     dist_low_bound = hist[1][x]
     dist_high_bound = hist[1][x+1]
@@ -67,8 +66,8 @@ for name in images:
                 #cv2.line(img2, (start_points[i][0], start_points[i][1]), (end_points[i][0],end_points[i][1]), (255,0,255),1) 
                 filtered_start_points.append(start_points[i])
                 filtered_end_points.append(end_points[i])
-    x1,y1,w,h = cv2.boundingRect(np.array(filtered_start_points))
-    cv2.rectangle(img2, (x1,y1), (x1+w, y1+h), (0,100,255),3)
+    x1,y1,w1,h1 = cv2.boundingRect(np.array(filtered_start_points))
+    cv2.rectangle(img2, (x1,y1), (x1+w1, y1+h1), (0,100,255),3)
 
     '''
     x2 = x1
@@ -83,8 +82,31 @@ for name in images:
     '''
     x2,y2,w2,h2 = cv2.boundingRect(np.array(filtered_end_points))
 
-    cv2.rectangle (img2, (x2,y2), (x2+w2, y2+h2), (100,255,0),3)
+    roi1 = img[y1:(y1+h1), x1:(x1+w1)]
+    roi2 = img[y2:(y2+h2), x2:(x2+w2)]
+    # resize the rois to half the average width/height
+    resize_width = int((w1+w2)/4)
+    resize_height  = int((h1+h2)/4)
+    resized_roi1 = cv2.resize(roi1, (resize_width, resize_height), interpolation = cv2.INTER_AREA )
+    resized_roi2 = cv2.resize(roi2, (resize_width, resize_height), interpolation = cv2.INTER_AREA )
+    
+    diffmask = cv2.absdiff(resized_roi1, resized_roi2)
+    diffmasksum = diffmask[:,:,0] + diffmask[:,:,1] + diffmask[:,:,2]
+    diffmaskdraw = np.zeros([resize_height, resize_width, 3],dtype=np.uint8)
+    diffmaskdraw[:,:,0] = diffmasksum
+    diffmaskdraw[:,:,2] = diffmasksum
 
-    cv2.imwrite('results/' + file_name + '_result.png', img2)
+    diffmaskdraw1 = cv2.resize(diffmaskdraw, (w1, h1), interpolation = cv2.INTER_CUBIC)
+    diffmaskdraw[:,:,2] = np.zeros([resize_height, resize_width],dtype=np.uint8)
+    diffmaskdraw[:,:,1] = diffmasksum
+
+    diffmaskdraw2 = cv2.resize(diffmaskdraw, (w2, h2), interpolation = cv2.INTER_CUBIC)
+
+    img2[y1:(y1+h1), x1:(x1+w1)] =  cv2.addWeighted(diffmaskdraw1, .5, roi1, 1, 0)
+    img2[y2:(y2+h2), x2:(x2+w2)] =  cv2.addWeighted(diffmaskdraw2, .5, roi2, 1, 0)
+
+    cv2.rectangle (img2, (x2,y2), (x2+w2, y2+h2), (100,255,0),3)
+    result = np.hstack((img, img2))
+    cv2.imwrite('results/' + file_name + '_result.png', result)
     print('===' + str(index) + '========================================')
     index += 1
